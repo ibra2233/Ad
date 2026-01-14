5edbff0b4f3865da4a18f00c7c468a2f31711a30
ibra2233 <35203482+ibra2233@users.noreply.github.com>
2026-01-14 22:53:35 +0100

    capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.logitrack.app',
+  appId: 'com.logitrack.admin',
   appName: 'LogiTrack',
   webDir: 'dist',
   bundledWebRuntime: false,
